0e7f8638962c634bce66e2b4013d0f4bf9ac0dd0
mustaphaabdulbasit35 <mustaphaabdulbasit335@gmail.com>
2024-02-29 19:08:45 +0000

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,3 @@
+v#update_header').on('click', function () {
+  $('header').text('New Header!!!');
+});
---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,4 @@
+
+://swapi.co/api/people/5/?format=json', (data) => {
+  $('DIV#character').text(data.name);
+});
